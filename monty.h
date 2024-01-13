632a1106456fe05c8a2c5f68386c73003bb387c9
elyessassi <mjoxen@gmail.com>
2024-01-13 12:19:39 +0100

    changes to header file

--- monty.h
+++ monty.h
@@ -50,6 +50,7 @@ int specif(char *line, stack_t *stack);
 void _pall(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void freestack(stack_t *stack);
+void _pint(stack_t **stack, unsigned int line_number);
 
 extern stack_t *head;
 
