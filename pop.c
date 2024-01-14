4f5cd667ed2770782d8dd508a856a19459873185
elyessassi <mjoxen@gmail.com>
2024-01-14 17:24:02 +0100

    fix pop

--- pop.c
+++ pop.c
@@ -12,7 +12,8 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free(temp);
+		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
