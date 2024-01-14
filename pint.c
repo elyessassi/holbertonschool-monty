76bc59ece3301d24ee2d141bf46fc6b31dbb4fe4
elyessassi <mjoxen@gmail.com>
2024-01-14 16:41:03 +0100

    fix betty

--- pint.c
+++ pint.c
@@ -1,19 +1,21 @@
 #include "monty.h"
 
 /**
- * 
-*/
+ * _pint - print top value
+ * @stack: top of stack
+ * @line_number: line number
+ */
 
 void _pint(stack_t **stack, unsigned int line_number)
 {
-    stack_t *node = *stack;
-    if (node != NULL)
-    {
-        printf("%d\n", node->n);
-    }
-    else
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	stack_t *node = *stack;
+	if (node != NULL)
+	{
+		printf("%d\n", node->n);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
