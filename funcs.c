e0f30a07e6b7196fd2aa6ed567f002be84299ac1
elyessassi <mjoxen@gmail.com>
2024-01-14 15:13:23 +0100

    fix

--- funcs.c
+++ funcs.c
@@ -19,7 +19,7 @@ void _push(stack_t **stack, unsigned int line_number)
     op = strtok(NULL, d);
     if ((op == NULL))
     {
-        fprintf(stderr, "L%d: usage: push integer", line_number);
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
     data = atoi(op);
