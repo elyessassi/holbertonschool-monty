516bb0f84ed153fafba16da27a734c0a261cb7e3
elyessassi <mjoxen@gmail.com>
2024-01-14 17:24:47 +0100

    fix pop

--- pop.c
+++ pop.c
@@ -13,7 +13,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		free(temp);
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
