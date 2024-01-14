3f3df5b4fe92c06a3f3686c683f39ff26d75673d
elyessassi <mjoxen@gmail.com>
2024-01-14 16:45:04 +0100

    betty

--- nop.c
+++ nop.c
@@ -1,11 +1,13 @@
 #include "monty.h"
 
 /**
- * 
+ * _nop - do nothing
+ * @stack: top of stack
+ * @line_number: line number
 */
 
 void _nop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
