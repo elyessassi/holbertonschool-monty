184f2d33aa662ff9573e2d56bc718d32ba7129d9
elyessassi <mjoxen@gmail.com>
2024-01-14 16:49:57 +0100

    betty

--- freestack.c
+++ freestack.c
@@ -1,18 +1,19 @@
 #include "monty.h"
 
 /**
- * 
-*/
+ * freestack - free stack
+ * @stack: top of stack
+ */
 
 void freestack(stack_t *stack)
 {
 	stack_t *current = stack;
-    stack_t *nextnode;
+	stack_t *nextnode;
 
 	while (current)
 	{
-        nextnode = current->next;
+		nextnode = current->next;
 		free(current);
-        current = nextnode;
+		current = nextnode;
 	}
 }
