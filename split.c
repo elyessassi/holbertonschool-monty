bae0cf0068ced99ca76726e2bdd5356ecb1ce26e
elyessassi <mjoxen@gmail.com>
2024-01-14 16:42:05 +0100

    fix betty split

--- split.c
+++ split.c
@@ -1,29 +1,31 @@
 #include "monty.h"
 
 /**
- *
+ * split - split line
+ * @stack: top of stack
+ * @line_number: line number
+ * @opcode: opcode
  */
 
 void split(stack_t **stack, unsigned int line_number, char *opcode)
 {
-    int i = 0;
-    instruction_t ops[] = {
-        {"push", _push},
-        {"pall", _pall},
-        {"pint", _pint},
-        {"nop", _nop},
-        {NULL, NULL}};
-    while (ops[i].opcode != NULL)
-    {
-
-        if (strcmp(opcode, ops[i].opcode) == 0)
-        {
-            ops[i].f(stack, line_number);
-            return;
-        }
-        i++;
-    }
-    if (strlen(opcode) != 0)
+	int i = 0;
+	instruction_t ops[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"nop", _nop},
+		{NULL, NULL}};
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(opcode, ops[i].opcode) == 0)
+		{
+			ops[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	if (strlen(opcode) != 0)
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 		exit(EXIT_FAILURE);
